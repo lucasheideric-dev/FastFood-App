441854c3a400a596ff3c0772042f8ac4c8a6b5fa
Lucas Heideric <56256816+LucasHeideric@users.noreply.github.com>
2025-02-18 21:33:42 -0300

    chore: add eslint sort plugin and pretter tailwindcss plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,8 +5,8 @@ const ProductPage = () => {
   return (
     <div>
       <h1 className="text-red-500">Products Page</h1>
-      <Button className="mx-5">Teste</Button>
-      <Input className="w-28" />
+      <Button className="mx-5 p-5">Teste</Button>
+      <Input className="w-28 p-5" />
     </div>
   );
 };
