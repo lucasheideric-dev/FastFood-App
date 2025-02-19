f84cdb5aee68fbf2908b67be386c835fa4b3645b
Lucas Heideric <56256816+LucasHeideric@users.noreply.github.com>
2025-02-18 21:13:11 -0300

    feat: shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductPage = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">Products Page</h1>
+      <Button className="mx-5">Teste</Button>
+      <Input className="w-28" />
+    </div>
+  );
+};
+
+export default ProductPage;
